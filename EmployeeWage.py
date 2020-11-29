3ff2323ffc2e9162ac6c7f40b9035d021122f204
Archana-Bhamare <archusonar2@gmail.com>
2020-11-29 10:57:24 +0530

    Added : Part Time Employee Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,29 @@
-print("Welcome TO Employee Wage Computation Program")
+import random
+
+
+class EmployeeWage:
+    IS_FULL_TIME = 1
+    IS_PART_TIME = 0
+    EMP_WAGE_PER_HOUR = 20
+    empHours = 0
+
+    def checkEmpAttendance(self):
+        attendance = random.randint(0, 2)
+        if attendance == EmployeeWage.IS_FULL_TIME:
+            EmployeeWage.empHours = 8
+            return "Employee is Present for Full Time"
+        elif attendance == EmployeeWage.IS_PART_TIME:
+            EmployeeWage.empHours = 4
+            return "Employee is Present for Part Time"
+        else:
+            return "Employee is Absent"
+
+    def calculateDailyEmpWage(self):
+        dailyWage = EmployeeWage.EMP_WAGE_PER_HOUR * EmployeeWage.empHours
+        return "Employee Daily Wage is : " + str(dailyWage)
+
+
+if __name__ == "__main__":
+    employee = EmployeeWage()
+    print(employee.checkEmpAttendance())
+    print(employee.calculateDailyEmpWage())
