3fb91e269b6db672fdd178e43793673f0f05854a
Archana-Bhamare <archusonar2@gmail.com>
2020-11-29 10:29:45 +0530

    Added : Calculate Daily Employee Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,26 @@
-print("Welcome TO Employee Wage Computation Program")
+import random
+
+
+class EmployeeWage:
+    EMP_WAGE_PER_HOUR = 20
+    empHours = 0
+
+    def checkEmpAttendance(self):
+        empcheck = random.randint(0, 1)
+        if empcheck == 1:
+            EmployeeWage.empHours = 8
+            return "Employee is Present"
+        else:
+            EmployeeWage.empHours = 0
+            return "Employee is absent"
+
+    def calculateDailyEmpWage(self):
+        dailyWage = EmployeeWage.EMP_WAGE_PER_HOUR * EmployeeWage.empHours
+        return  "Employee Daily Wage is : "+str(dailyWage)
+
+
+if __name__ == "__main__":
+    employee = EmployeeWage()
+    print(employee.checkEmpAttendance())
+    print(employee.calculateDailyEmpWage())
+
