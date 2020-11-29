58f8d9e24238d2dbaec5683e38300c2d2e317bca
Archana-Bhamare <archusonar2@gmail.com>
2020-11-29 14:39:58 +0530

    Added : Solving using case statement (Dictionary)

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,36 @@
-print("Welcome TO Employee Wage Computation Program")
+import random
+
+
+class EmployeeWage:
+    FULL_TIME_HOUR = 8
+    PART_TIME_HOUR = 4
+    IS_ABSENT = 0
+    EMP_WAGE_PER_HOUR = 20
+
+    randcheck = random.randint(0, 2)
+
+    def checkEmpAttendance(self):
+        if EmployeeWage.randcheck == 1:
+            return "Employee is Present for Full Time"
+        elif EmployeeWage.randcheck == 2:
+            return "Employee is Present for Part Time"
+        else:
+            return "Employee is Absent"
+
+    def empPerDayHours(self):
+        switch = {
+            0:EmployeeWage.IS_ABSENT,
+            1:EmployeeWage.FULL_TIME_HOUR,
+            2:EmployeeWage.PART_TIME_HOUR
+        }
+        return switch.get(EmployeeWage.randcheck)
+
+    def calculateDailyEmpWage(self,empHours):
+        dailyWage = EmployeeWage.EMP_WAGE_PER_HOUR * empHours
+        return "Employee Daily Wage is : " + str(dailyWage)
+
+
+if __name__ == "__main__":
+    employee = EmployeeWage()
+    print(employee.checkEmpAttendance())
+    print(employee.calculateDailyEmpWage(employee.empPerDayHours()))
